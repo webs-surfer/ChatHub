aa26c712cb9c1bbc113f82877069741d1a23828e
webs-surfer <itsrohanadhikari@gmail.com>
2026-03-06 05:16:43 +0530

    connection issue fix

--- Backend/frontend/src/api/axios.js
+++ Backend/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.PROD ? "" : "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
   },
@@ -10,15 +10,13 @@ API.interceptors.request.use(
   (req) => {
     const userInfo = localStorage.getItem("userInfo");
     if (userInfo) {
-      req.headers.Authorization = `Bearer ${
-        JSON.parse(userInfo).token
-      }`;
+      req.headers.Authorization = `Bearer ${JSON.parse(userInfo).token}`;
     }
     return req;
   },
   (error) => {
     return Promise.reject(error);
-  }
+  },
 );
 
 export default API;
